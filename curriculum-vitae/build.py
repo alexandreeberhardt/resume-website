ad0179cf41799f6ec9c207f81b57323f81cb2b1f
alexandre <alexandre.eber07@gmail.com>
2026-02-17 13:40:14 -0500

    remove unused files form backend

--- curriculum-vitae/build.py
+++
@@ -1,9 +0,0 @@
-from core import ResumeBuilder, ResumeConfig
-
-if __name__ == "__main__":
-    config = ResumeConfig(
-        yaml_path="data.yml", template_path="template.tex", output_tex_path="main.tex"
-    )
-
-    builder = ResumeBuilder(config)
-    builder.build()
